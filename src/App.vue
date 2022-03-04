550a3d87b2aa9f0b07b533174a29618cd7546d37
victorbohrer <de.bohrer@hotmail.com>
2022-03-04 14:39:01 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>{{titulo}}</h1>
+
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+</div>
+
+  
+</template>
+
+
+<script>
+
+export default {
+
+  data() {
+
+    return {
+      titulo: 'Lorem ipsum',
+      fotos: []
+    }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+    .then(res => res.json(), err => console.log(err))
+    .then(fotos => this.fotos = fotos)
+  }
+}
+
+</script>
+
+
+
+<style>
+
+ul {
+  display: flex;
+}
+
+img {
+  width: 100%;
+}
+
+</style>
